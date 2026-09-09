b7fe2bfd5709d944c125eaf944dac404acf2b673
Jiyoon Kim <jiyoonkim@Jiyoons-MacBook-Air.local>
2026-09-09 18:11:52 +0900

    feat: fix failing test

--- backend/domain/src/main/java/com/studyclub/domain/bookmark/StudyBookmarkRepository.java
+++ backend/domain/src/main/java/com/studyclub/domain/bookmark/StudyBookmarkRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface StudyBookmarkRepository extends JpaRepository<StudyBookmark, Long> {
     @Query(value = """
@@ -20,5 +21,6 @@ public interface StudyBookmarkRepository extends JpaRepository<StudyBookmark, Lo
 
     long countByAccountId(Long accountId);
 
+    @Transactional
     void deleteByAccountId(Long accountId);
 }
